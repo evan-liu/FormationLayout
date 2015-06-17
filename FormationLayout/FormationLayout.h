bd2c4846f8cd29b4306b06dd11ec46c9a198fe2a
Evan Liu <evancoding@gmail.com>
2015-06-18 11:43:45 +1200

    Create Project

---
+++ FormationLayout/FormationLayout.h
@@ -0,0 +1,19 @@
+//
+//  FormationLayout.h
+//  FormationLayout
+//
+//  Created by Evan on 18/06/15.
+//  Copyright © 2015 evancoding.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FormationLayout.
+FOUNDATION_EXPORT double FormationLayoutVersionNumber;
+
+//! Project version string for FormationLayout.
+FOUNDATION_EXPORT const unsigned char FormationLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FormationLayout/PublicHeader.h>
+
+
