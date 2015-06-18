764d6dcf57bb0af79d76c17df6273be7bfe395ab
Evan Liu <evancoding@gmail.com>
2015-06-18 19:12:38 +1200

    Add LayoutTarget and LayoutTargetTests

---
+++ FormationLayout/LayoutTarget.swift
@@ -0,0 +1,126 @@
+//
+//  ViewAttribute.swift
+//  FormationLayout
+//
+//  Created by Evan on 18/06/15.
+//  Copyright © 2015 evancoding.com. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - LayoutTarget
+
+public struct LayoutTarget : Equatable {
+    public let view: UIView
+    public let attribute: NSLayoutAttribute
+    
+    public var multiplier: CGFloat = 1
+    public var constant: CGFloat = 0
+    
+    public init(view: UIView, attribute: NSLayoutAttribute) {
+        self.view = view
+        self.attribute = attribute
+    }
+}
+
+// MARK: - Operators
+
+public func == (lhs: LayoutTarget, rhs: LayoutTarget) -> Bool {
+    return lhs.view == rhs.view && lhs.attribute == rhs.attribute && lhs.multiplier == rhs.multiplier && lhs.constant == rhs.constant
+}
+
+public func + (var lhs: LayoutTarget, rhs: CGFloat) -> LayoutTarget {
+    lhs.constant += rhs
+    return lhs
+}
+
+public func + (var lhs: LayoutTarget, rhs: Int) -> LayoutTarget {
+    lhs.constant += CGFloat(rhs)
+    return lhs
+}
+
+public func - (var lhs: LayoutTarget, rhs: CGFloat) -> LayoutTarget {
+    lhs.constant -= rhs
+    return lhs
+}
+
+public func - (var lhs: LayoutTarget, rhs: Int) -> LayoutTarget {
+    lhs.constant -= CGFloat(rhs)
+    return lhs
+}
+
+public func * (var lhs: LayoutTarget, rhs: CGFloat) -> LayoutTarget {
+    lhs.multiplier *= rhs
+    return lhs
+}
+
+public func * (var lhs: LayoutTarget, rhs: Int) -> LayoutTarget {
+    lhs.multiplier *= CGFloat(rhs)
+    return lhs
+}
+
+public func / (var lhs: LayoutTarget, rhs: CGFloat) -> LayoutTarget {
+    lhs.multiplier /= rhs
+    return lhs
+}
+
+public func / (var lhs: LayoutTarget, rhs: Int) -> LayoutTarget {
+    lhs.multiplier /= CGFloat(rhs)
+    return lhs
+}
+
+// MARK: - UIView extension
+
+public extension UIView {
+    public func layoutTarget(attribute: NSLayoutAttribute) -> LayoutTarget {
+        return LayoutTarget(view: self, attribute: attribute)
+    }
+    
+    public var left: LayoutTarget {
+        return LayoutTarget(view: self, attribute: .Left)
+    }
+    
+    public var right: LayoutTarget {
+        return LayoutTarget(view: self, attribute: .Right)
+    }
+    
+    public var top: LayoutTarget {
+        return LayoutTarget(view: self, attribute: .Top)
+    }
+    
+    public var bottom: LayoutTarget {
+        return LayoutTarget(view: self, attribute: .Bottom)
+    }
+    
+    public var leading: LayoutTarget {
+        return LayoutTarget(view: self, attribute: .Leading)
+    }
+    
+    public var trailing: LayoutTarget {
+        return LayoutTarget(view: self, attribute: .Trailing)
+    }
+    
+    public var width: LayoutTarget {
+        return LayoutTarget(view: self, attribute: .Width)
+    }
+    
+    public var height: LayoutTarget {
+        return LayoutTarget(view: self, attribute: .Height)
+    }
+    
+    public var centerX: LayoutTarget {
+        return LayoutTarget(view: self, attribute: .CenterX)
+    }
+    
+    public var centerY: LayoutTarget {
+        return LayoutTarget(view: self, attribute: .CenterY)
+    }
+    
+    public var baseline: LayoutTarget {
+        return LayoutTarget(view: self, attribute: .Baseline)
+    }
+    
+    public var firstBaseline: LayoutTarget {
+        return LayoutTarget(view: self, attribute: .FirstBaseline)
+    }
+}
---
+++ FormationLayoutTests/LayoutTargetTests.swift
@@ -0,0 +1,101 @@
+//
+//  LayoutTargetTests.swift
+//  FormationLayout
+//
+//  Created by Evan on 18/06/15.
+//  Copyright © 2015 evancoding.com. All rights reserved.
+//
+
+import XCTest
+import FormationLayout
+
+class LayoutTargetTests: XCTestCase {
+    let view = UIView()
+    
+    // view layout target factory methods
+    func testFactoryMethods() {
+        // should set view and attibute
+        
+        XCTAssertEqual(view.left.view, view)
+        XCTAssertEqual(view.left.attribute, NSLayoutAttribute.Left)
+        
+        XCTAssertEqual(view.right.view, view)
+        XCTAssertEqual(view.right.attribute, NSLayoutAttribute.Right)
+        
+        XCTAssertEqual(view.top.view, view)
+        XCTAssertEqual(view.top.attribute, NSLayoutAttribute.Top)
+        
+        XCTAssertEqual(view.bottom.view, view)
+        XCTAssertEqual(view.bottom.attribute, NSLayoutAttribute.Bottom)
+        
+        XCTAssertEqual(view.leading.view, view)
+        XCTAssertEqual(view.leading.attribute, NSLayoutAttribute.Leading)
+        
+        XCTAssertEqual(view.trailing.view, view)
+        XCTAssertEqual(view.trailing.attribute, NSLayoutAttribute.Trailing)
+        
+        XCTAssertEqual(view.width.view, view)
+        XCTAssertEqual(view.width.attribute, NSLayoutAttribute.Width)
+        
+        XCTAssertEqual(view.height.view, view)
+        XCTAssertEqual(view.height.attribute, NSLayoutAttribute.Height)
+        
+        XCTAssertEqual(view.centerX.view, view)
+        XCTAssertEqual(view.centerX.attribute, NSLayoutAttribute.CenterX)
+        
+        XCTAssertEqual(view.centerY.view, view)
+        XCTAssertEqual(view.centerY.attribute, NSLayoutAttribute.CenterY)
+        
+        XCTAssertEqual(view.baseline.view, view)
+        XCTAssertEqual(view.baseline.attribute, NSLayoutAttribute.Baseline)
+        
+        XCTAssertEqual(view.firstBaseline.view, view)
+        XCTAssertEqual(view.firstBaseline.attribute, NSLayoutAttribute.FirstBaseline)
+        
+        XCTAssertEqual(view.layoutTarget(.LeftMargin).view
+            , view)
+        XCTAssertEqual(view.layoutTarget(.LeftMargin).attribute
+            , NSLayoutAttribute.LeftMargin)
+        
+    }
+    
+    // == operator
+    func testEqual() {
+        var target1 = LayoutTarget(view: view, attribute: .Left)
+        var target2 = LayoutTarget(view: view, attribute: .Left)
+        
+        // should compare view
+        XCTAssert(target1 == target2)
+        XCTAssert(target1 != LayoutTarget(view: UIView(), attribute: .Left))
+        
+        // shold compare attribute
+        XCTAssert(target1 != LayoutTarget(view: view, attribute: .Right))
+        
+        // should compare multiplier
+        target1.multiplier = 2
+        XCTAssert(target1 != target2)
+        
+        target2.multiplier = 2
+        XCTAssert(target1 == target2)
+        
+        // should compare constant
+        target1.constant = 10
+        XCTAssert(target1 != target2)
+        
+        target2.constant = 10
+        XCTAssert(target1 == target2)
+    }
+    
+    // + - * / calculation operators
+    func testCalculation() {
+        // should take Int/CGFloat and change constant/multiplier
+        
+        XCTAssertEqual((view.left + 2 + 3.4).constant, 5.4)
+
+        XCTAssertEqual((view.left + 10 - 2 - 2.6).constant, 5.4)
+        
+        XCTAssertEqual((view.left * 2 * 1.5).multiplier, 3.0)
+        
+        XCTAssertEqual((view.left / 2 / 2.5).multiplier, 0.2)
+    }
+}
