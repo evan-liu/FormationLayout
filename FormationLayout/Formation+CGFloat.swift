b017748197c8a98bd1755bde15c2e4e03f135eb3
Evan Liu <evancoding@gmail.com>
2015-06-20 15:51:14 +1200

    Add protocol FormationTakesCGFloat.

---
+++ FormationLayout/Formation+CGFloat.swift
@@ -0,0 +1,82 @@
+//
+//  ViewFormation+CGFloat.swift
+//  FormationLayout
+//
+//  Created by Evan on 20/06/15.
+//  Copyright © 2015 evancoding.com. All rights reserved.
+//
+
+import UIKit
+
+public protocol FormationTakesCGFloat {
+    /// `NSLayoutConstraint` factory method with `CGFloat' constant.
+    func attribute(attribute: NSLayoutAttribute, relatedBy relation: NSLayoutRelation, constant: CGFloat, priority: UILayoutPriority, handler: ((NSLayoutConstraint) -> Void)?) -> Self
+}
+
+extension ViewFormation: FormationTakesCGFloat {
+    public func attribute(attribute: NSLayoutAttribute, relatedBy relation: NSLayoutRelation, constant: CGFloat, priority: UILayoutPriority = UILayoutPriorityRequired, handler: ((NSLayoutConstraint) -> Void)? = nil) -> Self {
+        addConstraint(NSLayoutConstraint(item: view, attribute: attribute, relatedBy: relation, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: constant), priority: priority, handler: handler)
+        return self
+    }
+}
+
+extension FormationTakesCGFloat {
+    public func width(constant: CGFloat, priority: UILayoutPriority = UILayoutPriorityRequired, handler: ((NSLayoutConstraint) -> Void)? = nil) -> Self {
+        attribute(.Width, relatedBy: .Equal, constant: constant, priority: priority, handler: handler)
+        return self
+    }
+    public func widthEqual(constant: CGFloat, priority: UILayoutPriority = UILayoutPriorityRequired, handler: ((NSLayoutConstraint) -> Void)? = nil) -> Self {
+        attribute(.Width, relatedBy: .Equal, constant: constant, priority: priority, handler: handler)
+        return self
+    }
+    public func widthLessThanOrEqual(constant: CGFloat, priority: UILayoutPriority = UILayoutPriorityRequired, handler: ((NSLayoutConstraint) -> Void)? = nil) -> Self {
+        attribute(.Width, relatedBy: .LessThanOrEqual, constant: constant, priority: priority, handler: handler)
+        return self
+    }
+    public func widthGreaterThanOrEqual(constant: CGFloat, priority: UILayoutPriority = UILayoutPriorityRequired, handler: ((NSLayoutConstraint) -> Void)? = nil) -> Self {
+        attribute(.Width, relatedBy: .GreaterThanOrEqual, constant: constant, priority: priority, handler: handler)
+        return self
+    }
+}
+
+extension FormationTakesCGFloat {
+    public func height(constant: CGFloat, priority: UILayoutPriority = UILayoutPriorityRequired, handler: ((NSLayoutConstraint) -> Void)? = nil) -> Self {
+        attribute(.Height, relatedBy: .Equal, constant: constant, priority: priority, handler: handler)
+        return self
+    }
+    public func heightEqual(constant: CGFloat, priority: UILayoutPriority = UILayoutPriorityRequired, handler: ((NSLayoutConstraint) -> Void)? = nil) -> Self {
+        attribute(.Height, relatedBy: .Equal, constant: constant, priority: priority, handler: handler)
+        return self
+    }
+    public func heightLessThanOrEqual(constant: CGFloat, priority: UILayoutPriority = UILayoutPriorityRequired, handler: ((NSLayoutConstraint) -> Void)? = nil) -> Self {
+        attribute(.Height, relatedBy: .LessThanOrEqual, constant: constant, priority: priority, handler: handler)
+        return self
+    }
+    public func heightGreaterThanOrEqual(constant: CGFloat, priority: UILayoutPriority = UILayoutPriorityRequired, handler: ((NSLayoutConstraint) -> Void)? = nil) -> Self {
+        attribute(.Height, relatedBy: .GreaterThanOrEqual, constant: constant, priority: priority, handler: handler)
+        return self
+    }
+}
+
+extension FormationTakesCGFloat {
+    public func size(constant: CGFloat, priority: UILayoutPriority = UILayoutPriorityRequired, handler: ((NSLayoutConstraint) -> Void)? = nil) -> Self {
+        attribute(.Width, relatedBy: .Equal, constant: constant, priority: priority, handler: handler)
+        attribute(.Height, relatedBy: .Equal, constant: constant, priority: priority, handler: handler)
+        return self
+    }
+    public func sizeEqual(constant: CGFloat, priority: UILayoutPriority = UILayoutPriorityRequired, handler: ((NSLayoutConstraint) -> Void)? = nil) -> Self {
+        attribute(.Width, relatedBy: .Equal, constant: constant, priority: priority, handler: handler)
+        attribute(.Height, relatedBy: .Equal, constant: constant, priority: priority, handler: handler)
+        return self
+    }
+    public func sizeLessThanOrEqual(constant: CGFloat, priority: UILayoutPriority = UILayoutPriorityRequired, handler: ((NSLayoutConstraint) -> Void)? = nil) -> Self {
+        attribute(.Width, relatedBy: .LessThanOrEqual, constant: constant, priority: priority, handler: handler)
+        attribute(.Height, relatedBy: .LessThanOrEqual, constant: constant, priority: priority, handler: handler)
+        return self
+    }
+    public func sizeGreaterThanOrEqual(constant: CGFloat, priority: UILayoutPriority = UILayoutPriorityRequired, handler: ((NSLayoutConstraint) -> Void)? = nil) -> Self {
+        attribute(.Width, relatedBy: .GreaterThanOrEqual, constant: constant, priority: priority, handler: handler)
+        attribute(.Height, relatedBy: .GreaterThanOrEqual, constant: constant, priority: priority, handler: handler)
+        return self
+    }
+}
