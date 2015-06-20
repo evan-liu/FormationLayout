0cdfb95c1fd568ca929f0a9509fb4b0009a1cb15
Evan Liu <evancoding@gmail.com>
2015-06-21 10:37:56 +1200

    Add Formation+Position helper methods

---
+++ FormationLayout/Formation+Position.swift
@@ -0,0 +1,79 @@
+//
+//  Formation+Position.swift
+//  FormationLayout
+//
+//  Created by Evan on 21/06/15.
+//  Copyright © 2015 evancoding.com. All rights reserved.
+//
+
+import Foundation
+
+extension FormationTakesLayoutTarget {
+    /// layout.view(v1).above(v2)
+    /// - == layout.view(v1).bottom(v2.top)
+    /// - == v1.Bottom = v2.Top
+    public func above(secondView: UIView, priority: UILayoutPriority = UILayoutPriorityRequired, handler: ((NSLayoutConstraint) -> Void)? = nil) -> Self {
+        let target = LayoutTarget(view: secondView, attribute: .Top)
+        attribute(.Bottom, relatedBy: .Equal, target:target, priority: priority, handler: handler)
+        return self
+    }
+    /// layout.view(v1).below(v2)
+    /// - == layout.view(v1).top(v2.bottom)
+    /// - == v1.Top = v2.Bottom
+    public func below(secondView: UIView, priority: UILayoutPriority = UILayoutPriorityRequired, handler: ((NSLayoutConstraint) -> Void)? = nil) -> Self {
+        let target = LayoutTarget(view: secondView, attribute: .Bottom)
+        attribute(.Top, relatedBy: .Equal, target:target, priority: priority, handler: handler)
+        return self
+    }
+    /// layout.view(v1).before(v2) 
+    /// - == layout.view(v1).trailing(v2.leading)
+    /// - == v1.Trailing = v2.Leading
+    public func before(secondView: UIView, priority: UILayoutPriority = UILayoutPriorityRequired, handler: ((NSLayoutConstraint) -> Void)? = nil) -> Self {
+        let target = LayoutTarget(view: secondView, attribute: .Leading)
+        attribute(.Trailing, relatedBy: .Equal, target:target, priority: priority, handler: handler)
+        return self
+    }
+    /// layout.view(v1).after(v2) 
+    /// - == layout.view(v1).leading(v2.trailing)
+    /// - == v1.Leading = v2.Trailing
+    public func after(secondView: UIView, priority: UILayoutPriority = UILayoutPriorityRequired, handler: ((NSLayoutConstraint) -> Void)? = nil) -> Self {
+        let target = LayoutTarget(view: secondView, attribute: .Trailing)
+        attribute(.Leading, relatedBy: .Equal, target:target, priority: priority, handler: handler)
+        return self
+    }
+}
+
+extension FormationTakesLayoutTarget {
+    /// layout.view(v1).above(v2 - 10) 
+    /// - == layout.view(v1).bottom(v2.top - 10)
+    /// - == v1.Bottom = v2.Top - 10
+    public func above(var target: LayoutTarget, priority: UILayoutPriority = UILayoutPriorityRequired, handler: ((NSLayoutConstraint) -> Void)? = nil) -> Self {
+        target.attribute = .Top
+        attribute(.Bottom, relatedBy: .Equal, target: target, priority: priority, handler: handler)
+        return self
+    }
+    /// layout.view(v1).below(v2 + 10)
+    /// - == layout.view(v1).top(v2.bottom + 10)    
+    /// - == v1.Top = v2.Bottom + 10
+    public func below(var target: LayoutTarget, priority: UILayoutPriority = UILayoutPriorityRequired, handler: ((NSLayoutConstraint) -> Void)? = nil) -> Self {
+        target.attribute = .Bottom
+        attribute(.Top, relatedBy: .Equal, target: target, priority: priority, handler: handler)
+        return self
+    }
+    /// layout.view(v1).before(v2 - 10)
+    /// - == layout.view(v1).trailing(v2.leading - 10)
+    /// - == v1.Trailing = v2.Leading - 10
+    public func before(var target: LayoutTarget, priority: UILayoutPriority = UILayoutPriorityRequired, handler: ((NSLayoutConstraint) -> Void)? = nil) -> Self {
+        target.attribute = .Leading
+        attribute(.Trailing, relatedBy: .Equal, target: target, priority: priority, handler: handler)
+        return self
+    }
+    /// layout.view(v1).after(v2 + 10) 
+    /// - == layout.view(v1).leading(v2.trailing + 10)
+    /// - == v1.Leading = v2.Trailing + 10
+    public func after(var target: LayoutTarget, priority: UILayoutPriority = UILayoutPriorityRequired, handler: ((NSLayoutConstraint) -> Void)? = nil) -> Self {
+        target.attribute = .Trailing
+        attribute(.Leading, relatedBy: .Equal, target: target, priority: priority, handler: handler)
+        return self
+    }
+}
---
+++ FormationLayoutTests/Formation+PositionTests.swift
@@ -0,0 +1,68 @@
+//
+//  Formation+PositionTests.swift
+//  FormationLayout
+//
+//  Created by Evan on 21/06/15.
+//  Copyright © 2015 evancoding.com. All rights reserved.
+//
+
+import XCTest
+@testable import FormationLayout
+
+class Formation_PositionTests: XCTestCase {
+    var view: UIView!
+    var formation: ViewFormation!
+    
+    var view2: UIView!
+    
+    var checked = 0
+    
+    override func setUp() {
+        super.setUp()
+        
+        view = UIView()
+        formation = ViewFormation(view: view)
+        
+        view2 = UIView()
+        
+        checked = 0
+    }
+    
+    func testUIView() {
+        formation.above(view2) { self.check($0, .Bottom, .Top) }
+        formation.below(view2) { self.check($0, .Top, .Bottom) }
+        formation.before(view2) { self.check($0, .Trailing, .Leading) }
+        formation.after(view2) { self.check($0, .Leading, .Trailing) }
+        
+        verifyChecked(4)
+    }
+    
+    func testLayoutTarget() {
+        formation.above(view2 + 10) { self.check($0, .Bottom, .Top, 1, 10) }
+        formation.below(view2 - 10) { self.check($0, .Top, .Bottom, 1, -10) }
+        formation.before(view2 + 5.5) { self.check($0, .Trailing, .Leading, 1, 5.5) }
+        formation.after(view2 - 5.5) { self.check($0, .Leading, .Trailing, 1, -5.5) }
+        
+        verifyChecked(4)
+    }
+    
+    private func check(constraint: NSLayoutConstraint, _ firstAttribute: NSLayoutAttribute, _ secondAttribute: NSLayoutAttribute, _ multiplier: CGFloat = 1, _ constant: CGFloat = 0) {
+        
+        XCTAssertEqual(constraint.firstItem as! UIView, view)
+        XCTAssertEqual(constraint.secondItem as! UIView, view2)
+        
+        XCTAssertEqual(constraint.firstAttribute, firstAttribute)
+        XCTAssertEqual(constraint.secondAttribute, secondAttribute)
+        
+        XCTAssertEqual(constraint.relation, .Equal)
+        
+        XCTAssertEqual(constraint.multiplier, multiplier)
+        XCTAssertEqual(constraint.constant, constant)
+        
+        checked++
+    }
+    
+    private func verifyChecked(v: Int) {
+        XCTAssertEqual(checked, v)
+    }
+}
