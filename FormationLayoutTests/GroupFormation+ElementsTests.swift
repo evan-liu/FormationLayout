c4946b7041265ce1ce47f3b3b8a8a30d492d9f72
Evan Liu <evancoding@gmail.com>
2015-06-22 21:38:55 +1200

    GroupFormation: firstTop() firstLeading() lastBottom() lastTrailing()

--- FormationLayoutTests/GroupFormation+ElementsTests.swift
+++ FormationLayoutTests/GroupFormation+ElementsTests.swift
@@ -41,4 +41,35 @@ class GroupFormation_ElementsTests: XCTestCase {
         XCTAssertEqual(group.viewAt(1), view2)
         XCTAssertEqual(group.viewAt(2), view3)
     }
+    
+    func testSpecialElements() {
+        let view4 = UIView()
+        
+        var checked = 0
+        func checkConstraint(constraint: NSLayoutConstraint, _ firstView: UIView, _ firstAttribute: NSLayoutAttribute, _ secondView: UIView, secondAttribute: NSLayoutAttribute, _ constant: CGFloat = 0) {
+            XCTAssertEqual(constraint.firstItem as! UIView, firstView)
+            XCTAssertEqual(constraint.firstAttribute, firstAttribute)
+            
+            XCTAssertEqual(constraint.secondItem as! UIView, secondView)
+            XCTAssertEqual(constraint.secondAttribute, secondAttribute)
+            
+            XCTAssertEqual(constraint.constant, constant)
+            
+            checked++
+        }
+
+        group.firstLeading(view4) { checkConstraint($0, self.view1, .Leading, view4, secondAttribute: .Leading) }
+        group.firstLeading(view4 + 10) { checkConstraint($0, self.view1, .Leading, view4, secondAttribute: .Leading, 10) }
+
+        group.firstTop(view4) { checkConstraint($0, self.view1, .Top, view4, secondAttribute: .Top) }
+        group.firstTop(view4 + 10) { checkConstraint($0, self.view1, .Top, view4, secondAttribute: .Top, 10) }
+        
+        group.lastTrailing(view4) { checkConstraint($0, self.view3, .Trailing, view4, secondAttribute: .Trailing) }
+        group.lastTrailing(view4 + 10) { checkConstraint($0, self.view3, .Trailing, view4, secondAttribute: .Trailing, 10) }
+        
+        group.lastBottom(view4) { checkConstraint($0, self.view3, .Bottom, view4, secondAttribute: .Bottom) }
+        group.lastBottom(view4 + 10) { checkConstraint($0, self.view3, .Bottom, view4, secondAttribute: .Bottom, 10) }
+        
+        XCTAssertEqual(checked, 8)
+    }
 }
