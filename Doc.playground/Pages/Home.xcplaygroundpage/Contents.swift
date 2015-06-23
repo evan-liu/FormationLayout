b08b184b08d3e42934fcd13767a9223ccad220c4
Evan Liu <evancoding@gmail.com>
2015-06-23 20:16:06 +1200

    [Doc] Table of Content

--- Doc.playground/Pages/Home.xcplaygroundpage/Contents.swift
+++ Doc.playground/Pages/Home.xcplaygroundpage/Contents.swift
@@ -4,10 +4,16 @@
 
 ![banner](banner.png)
 
+**Note** Please open Doc.xcworkspace instead of Doc.playground itself.
+
 ## Table of Content
 
 - [FormationLayout](FormationLayout)
 - [LayoutTarget](LayoutTarget)
+- [FactoryMethods](FactoryMethods)
+- [Constraints](Constraints)
+- [GroupFormation](GroupFormation)
+- [SizeClass](SizeClass)
 
 ----
 [Next](@next)
