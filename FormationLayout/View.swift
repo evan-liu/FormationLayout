732fcdfddd17f79b23897a2dea16ddbc3c5f6861
Evan Liu <evancoding@gmail.com>
2015-06-29 10:44:43 +1200

    Add Doc comments to View

--- FormationLayout/View.swift
+++ FormationLayout/View.swift
@@ -8,15 +8,21 @@
 
 import Foundation
 
-/// Type protocol for views.
+/// Type protocol for views. A `View` can be an instance of `UIView` or `UILayoutGuide` (and `NSView` for OSX in the furture).
 public protocol View {
     
+    /// Prepare the View to work with the auto layout system. For `UIView` it is to set `translatesAutoresizingMaskIntoConstraints` to true .
     func prepareForLayout()
     
+    /// Check if the view is equal to another.
     func isEqualTo(other: View) -> Bool
     
+    /// Container of the view. `superView` for `UIView' and `owningView` for `UILayoutGuide`.
     var container: Container? { get }
     
+    /// Add the view to a container. 
+    /// - Parameter container: The container for the view. 
+    /// - Parameter forceMove: If move the view to the new container if the view is already in a container.
     func addToContainer(container: Container, forceMove: Bool)
     
 }
