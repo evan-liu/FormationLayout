abbb3abdbaafb64103565fa4c0bec8a22670a37d
Evan Liu <evancoding@gmail.com>
2015-07-05 20:55:56 +1200

    Create stack view demo

---
+++ Demo/StackViewDemo/StackViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackViewDemo
+//
+//  Created by Evan on 5/07/15.
+//  Copyright © 2015 evancoding.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
